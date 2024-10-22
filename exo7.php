97ae200654168397429752b00434d60bd893304a
Yopetito <Yofer.ruffo@gmail.com>
2024-10-22 15:05:45 +0200

    Exercice 7 complet

--- exo7.php
+++ exo7.php
@@ -6,3 +6,16 @@
 
 <?php
 
+$age = 10;
+
+if($age >= 6 && $age <= 7) {   
+    echo "L'enfant qui a $age ans appartient à la catégorie Poussin<br>";
+}   elseif($age >= 8 && $age <= 9) {
+        echo "L'enfant qui a $age ans appartient à la catégorie Pupille<br>";
+    } elseif ($age >= 10 && $age <= 11) {
+        echo "L'enfant qui a $age ans appartient à la catégorie Minime<br>";
+    } elseif($age >= 12) {
+        echo "L'enfant qui a $age ans appartient à la catégorie Cadet<br>";
+    } else {
+        echo "La catégorie n'et pas gérée!";
+    }
