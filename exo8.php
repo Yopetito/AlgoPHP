6a20f21a205069cf2d58986c51f46255a6d45f52
Yopetito <Yofer.ruffo@gmail.com>
2024-10-22 16:23:15 +0200

    Exo8 Avec d'autre boucles

--- exo8.php
+++ exo8.php
@@ -6,12 +6,35 @@
 
 <?php
 
-$nb = 5; //chiffre a multiplier
-$count = 1; //compteur de 1 a 10
-
+// WHILE
+echo "Resultat 1 with while: <br>";
+$nb = 5;
+$count = 1;
 echo "Table de $nb : <br>";
 while($count <= 10) {
     $result = $nb * $count; 
     echo "$nb x $count = $result <br>";
     $count++;
+}
+
+//FOR
+
+echo "Resultat 2 with FOR: <br>";
+$nb2 = 4;
+
+echo "Table de $nb2 : <br>";
+for($i = 1; $i <= 10; $i++) {
+    $result = $nb * $i; 
+    echo "$nb x $i = $result <br>";
+}
+
+//FOREACH
+
+Echo "Resultat 3 with foreach: <br>";
+$nb = 3;
+$table = range(1, 10);
+echo "Table de $nb : <br>";
+foreach($table as $i) {
+    $result = $nb * $i;
+    echo "$nb x $i = $result <br>";
 }
---
+++ exo9.php
@@ -0,0 +1,8 @@
+<h1>>Exercice 8</h1>
+
+<p>Ecrire un algorithme qui renvoie la table de multiplication d'un nombre passé en paramètre<br></p>
+
+<h2>>Résultat</h2>
+
+<?php
+
